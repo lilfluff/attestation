2e3067d4d80aad1b2edcbbc436d6b2e0ae0a8614
Anastasiia <aktlnkv@gmail.com>
2023-02-19 17:48:57 +0500

    Вторая часть задачи

--- Attestation/Program.cs
+++ Attestation/Program.cs
@@ -1,11 +1,24 @@
-﻿string [] array = {"one", "two", "three", "four", "five"};
+﻿string[] array = { "one", "two", "three", "four", "five"};
 PrintArray(array);
+Console.WriteLine();
+SortByThree(array);
 
-
-void PrintArray (string [] array)
+void PrintArray(string[] array)
 {
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write("{1} ", i, array[i]);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("{1} ", i, array[i]);
+    }
 }
+
+void SortByThree(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length == 3)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        else continue;
+    }
 }
