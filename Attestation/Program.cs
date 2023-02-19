d1226a76658d751dc955f086251a35d9ba6d8fa2
Anastasiia <aktlnkv@gmail.com>
2023-02-19 17:42:56 +0500

    Первая часть задачи

---
+++ Attestation/Program.cs
@@ -0,0 +1,11 @@
+﻿string [] array = {"one", "two", "three", "four", "five"};
+PrintArray(array);
+
+
+void PrintArray (string [] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("{1} ", i, array[i]);
+}
+}
